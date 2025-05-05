5758d46f63894dd3810499cf320cdceb46ccc6be
Camilo Pinzon <pinzonac@gmail.com>
2025-05-05 12:14:26 -0500

    Server side validation, client validation checkSession helpper method applied to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,21 @@
 'use client';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { checkSession } from '@/utils/checkSession';
+
 const Dasboard = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    checkSession().then((session) => {
+      if (!session) {
+        router.push('/login');
+      }
+    });
+  }, [router]);
+
   return <h1>Dashboard</h1>;
 };
 
