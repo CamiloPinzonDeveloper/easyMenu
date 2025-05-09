32d613a22b27bc417d3c05edaa1f868b8c69a826
Camilo Pinzon <pinzonac@gmail.com>
2025-05-09 00:18:16 -0500

    partial section, missing styles

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,8 +1,60 @@
+import Image from 'next/image';
+
+import styles from './Nosotros.module.scss';
+
 const NosotrosPage = () => {
   return (
-    <div>
-      <h1>Nosotros</h1>
-      <p>Esta es la página de nosotros.</p>
+    <div className={styles.nosotros}>
+      <section className={`wideContainer ${styles.discover}`}>
+        <div>
+          <h1>Descubre el viaje de EasyMenu</h1>
+          <p>
+            Fundada en 2025, EasyMenu nació de una pasión por mejorar las experiencias gastronómicas
+            a través de la tecnología. Nuestra misión es brindar a los propietarios de restaurantes
+            soluciones innovadoras que simplifiquen la gestión del menú y eleven la satisfacción de
+            los clientes.
+          </p>
+          <div>
+            <div>
+              <div>100%</div>
+              <div>
+                Nos dedicamos a la mejora continua y a la innovación en la industria de
+                restaurantes.
+              </div>
+            </div>
+            <div>
+              <div>Enfocados</div>
+              <div>
+                Nuestro enfoque centrado en el usuario garantiza que priorizamos sus comentarios y
+                necesidades.
+              </div>
+            </div>
+            <div>
+              <div>Valores</div>
+              <div>
+                La integridad y la transparencia están en el corazón de nuestras operaciones.
+              </div>
+            </div>
+            <div>
+              <div>Trabajo en equipo</div>
+              <div>
+                Nuestro espíritu de colaboración nos impulsa a diseñar soluciones a la medida para
+                los propietarios de restaurantes.
+              </div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <Image
+            src="/discover.webp"
+            alt="Equipo de EasyMenu trabajando en una mesa"
+            width={512}
+            height={512}
+            loading="eager"
+            priority={true}
+          />
+        </div>
+      </section>
     </div>
   );
 };
